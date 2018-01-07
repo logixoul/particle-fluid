62107daed123b4eb380623668a11098575ae5cf3
Stefan Monov <logixoul@gmail.com>
2018-01-08 01:59:55 +0200

    works now

--- src/main4.cpp
+++ src/main4.cpp
@@ -7,29 +7,21 @@ struct SApp : App {
 	void setup()
 	{
 		setWindowSize(sx, sy);
-
-		vector<vec2> poss(sx*sy);
-		for(int i = 0; i < poss.size(); i++) {
-			poss[i] = vec2(i%sx, i / sx);
-		}
-		gl::VboRef vbo = gl::Vbo::create(GL_ARRAY_BUFFER, poss, GL_STATIC_DRAW);
-		geom::BufferLayout posLayout;
-		posLayout.append(geom::POSITION, 2, sizeof(decltype(poss[0])), 0);
-		vboMesh = gl::VboMesh::create(poss.size(), GL_POINTS,
-			{ std::make_pair(posLayout, vbo) }
-		);
 	}
 	void draw()
 	{
 		gl::clear(Color(0, 0, 0));
-
+		gl::setMatricesWindow(sx, sy);
+		
 		static string vshader =
 			"#version 450\n"
-			"in vec2 ciPosition;"
+			"uniform mat4 proj;"
+			"in vec4 ciPosition;"
 			"void main() {"
-			"	gl_Position.xy = ciPosition.xy;"
+			/*"	gl_Position.xy = ciPosition.xy;"
 			"	gl_Position.z = 0.0;"
-			"	gl_Position.w = 1.0;"
+			"	gl_Position.w = 1.0;"*/
+			"	gl_Position = proj * ciPosition;"
 			"}"
 			;
 		static string fshader =
@@ -43,8 +35,19 @@ struct SApp : App {
 			gl::GlslProg::Format().vertex(vshader).fragment(fshader).attribLocation("ciPosition", 0).preprocess(false));
 
 		gl::ScopedGlslProg sgp(prog);
-					
+		auto proj = gl::context()->getProjectionMatrixStack().back();
+		prog->uniform("proj", proj);
 		glPointSize(30);
+		vector<vec2> poss(sx*sy);
+		for (int i = 0; i < poss.size(); i++) {
+			poss[i] = vec2(i%sx, i / sx);
+		}
+		gl::VboRef vbo = gl::Vbo::create(GL_ARRAY_BUFFER, poss, GL_STATIC_DRAW);
+		geom::BufferLayout posLayout;
+		posLayout.append(geom::POSITION, 2, sizeof(decltype(poss[0])), 0);
+		vboMesh = gl::VboMesh::create(poss.size(), GL_POINTS,
+		{ std::make_pair(posLayout, vbo) }
+		);
 		gl::draw(vboMesh);
 		gl::checkError();
 	}
