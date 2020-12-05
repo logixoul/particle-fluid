5d478075aff182e44552e60b16a521682aaf30bf
Stefan Monov <logixoul@gmail.com>
2020-12-05 12:18:27 +0200

    buildtimes. also compilation fix

--- src/cfg1.cpp
+++ src/cfg1.cpp
@@ -2,3 +2,28 @@
 #include "cfg1.h"
 
 std::map<string,cfg1::Opt> cfg1::opts;
+
+float cfg1::getOpt(string name, float defaultVal, std::function<bool()> shouldUpdate, std::function<float()> getVal) {
+	if (opts.find(name) == opts.end()) {
+		Opt opt1;
+		opt1.name = name;
+		opt1.shouldUpdate = shouldUpdate;
+		opt1.getVal = getVal;
+		opt1.val = defaultVal;
+		opts[name] = opt1;
+	}
+	auto& opt = opts[name];
+	if (opt.shouldUpdate()) {
+		opt.val = opt.getVal();
+	}
+	return opt.val;
+}
+
+void cfg1::print() {
+	cout << "============ CFG values ============";
+	for (auto& pair : opts) {
+		auto& key = pair.first;
+		auto opt = opts[key];
+		cout << opt.name << " = " << opt.val;
+	}
+}
--- src/cfg1.h
+++ src/cfg1.h
@@ -4,29 +4,8 @@
 class cfg1
 {
 public:
-	static float getOpt(string name, float defaultVal, std::function<bool()> shouldUpdate, std::function<float()> getVal) {
-		if(opts.find(name) == opts.end()) {
-			Opt opt1;
-			opt1.name=name;
-			opt1.shouldUpdate=shouldUpdate;
-			opt1.getVal=getVal;
-			opt1.val=defaultVal;
-			opts[name]=opt1;
-		}
-		auto& opt = opts[name];
-		if(opt.shouldUpdate()) {
-			opt.val=opt.getVal();
-		}
-		return opt.val;
-	}
-	static void print() {
-		cout << "============ CFG values ============";
-		for(auto& pair: opts) {
-			auto& key=pair.first;
-			auto opt = opts[key];
-			cout << opt.name << " = " << opt.val;
-		}
-	}
+	static float getOpt(string name, float defaultVal, std::function<bool()> shouldUpdate, std::function<float()> getVal);
+	static void print();
 private:
 	struct Opt
 	{
